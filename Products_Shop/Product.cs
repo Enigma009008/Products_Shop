ed81db4cdb034e667744d31e65a6457cd7d9b8df
Murad_777 <murad.gadzhiev1999@mail.ru>
2021-11-21 14:16:49 +0300

    version 1.0

---
+++ Products_Shop/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products_Shop
+{
+    public class Product
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Discount { get; set; }
+
+        public Product(string title, string description, string discount)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Discount = discount;
+
+        }
+
+        public string GetInfo()
+        {
+            if (!string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(Description) && !string.IsNullOrEmpty(Discount))
+            {
+                return $"Наименование: { Title} Описание: {Description}  Скидка: { Discount}";
+            }
+            else
+            {
+                return "Нет данных!";
+            }
+        }
+
+
+    }
+}
---
+++ Products_Shop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Products_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product("Вишня","Полезная ягода","5%"),
+                new Product("Яблоко","Полезный фрукт","10%"),
+                new Product("Огурец","Полезный овощ","15%"),
+                new Product("Абрикос","Полезный фрукт","10%"),
+                new Product("Авокадо","Полезный фрукт","5%"),
+                new Product("Ананас","Полезный фрукт","15%"),
+
+
+            };
+
+            Console.WriteLine("Скидки есть - 5%  10%  15%");
+            Console.Write("Введите скидку:");
+            string searchText = Console.ReadLine();
+
+            var search = products.Where(item => item.Discount.Contains(searchText));
+
+            Console.WriteLine("Результат поиска:");
+            Console.WriteLine($"Количество: { search.Count()}");
+            foreach(var item in search)
+            {
+                Console.WriteLine(item.GetInfo());
+            }
+          
+
+        }
+    }
+}
